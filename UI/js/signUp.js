6081202dd829923eb292cbdfbf93995bd293a79c
Ezeokafor <ezeokafortochukwu@gmail.com>
2019-06-21 16:48:47 +0100

    166834147-chore(finalCheck):     * Did final check on all pages to ensure all features are working properly
        [Finishes #166834147]

---
+++ UI/js/signUp.js
@@ -0,0 +1,34 @@
+const signup_btn = document.querySelector("#signupBtn");
+
+signup_btn.addEventListener('click', () => {
+
+    const first_name = document.querySelector("#firstname").value;
+    const last_name = document.querySelector("#lastname").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    const confirm_password = document.querySelector("#confirm-password").value;
+
+    const userInfo = {
+        first_name,
+        last_name,
+        email,
+        password,
+        confirm_password
+    }
+
+    const formData = new FormData();
+
+    formData.append('fname', userInfo.first_name);
+    formData.append('lname', userInfo.last_name);
+
+    fetch('https://my-json-server.typicode.com/typicode', {
+        method: 'post',
+        data: formData
+    })
+    .then(response => {
+        if(response.status === '200') {
+        console.log(response);
+        }
+    })
+
+});
