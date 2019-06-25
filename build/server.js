5a25a38b0dda96df10ffd2aae25b1786810d6529
Ezeokafor <ezeokafortochukwu@gmail.com>
2019-06-25 22:32:11 +0100

    166897731-chore(setup):     * Installed package.json file     * Installed package-lock.json file     * Installed babel and set up .babelrc file     * Installed eslint and set up .eslintrc.json file     * Installed Travis CI and set up .travis.yml file

---
+++ build/server.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+
+
+// eslint-disable-next-line no-underscore-dangle
+// eslint-disable-next-line no-use-before-define
+const _express = _interopRequireDefault(require('express'));
+
+// eslint-disable-next-line no-underscore-dangle
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// eslint-disable-next-line vars-on-top
+const app = (0, _express.default)();
+app.use(_express.default.json());
+app.get('/', (_req, res) => res.status(200).send({
+  message: 'YAY! Congratulations! Your first endpoint is working',
+}));
+app.listen(3000);
+console.log('app running on port ', 3000);
