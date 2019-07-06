dabcca4e30e11999d4809f756bef991f0534426b
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2019-07-06 16:28:08 +0100

    Update agent.js

--- src/controllers/agent.js
+++ src/controllers/agent.js
@@ -16,41 +16,41 @@ const agentController = {
     }
     const agent = new agentModel(req.body);
     database.using(agent).write('insert')
-    return res.status(201).send({ agent });
+    return res.status(201).send({ agent:agent.attributes });
   },
 
 
   getAll(req, res) {
     const agents = database.read(agentModel)._rows.map((row) => { return row.attributes; })
-    return res.status(200).send(agents);
+    return res.status(200).send({ agents });
   },
 
 
   getOne(req, res) {
-    const oneAgent = database.read(agentModel, {id:req.param.id}).first();
+    const oneAgent = database.read(agentModel, {id:parseInt(req.param.id)}).first();
     if (!oneAgent) {
       return res.status(404).send({ message: 'agent not found' });
     }
-    return res.status(200).send(oneAgent.attributes);
+    return res.status(200).send({ agent: oneAgent.attributes });
   },
 
 
   update(req, res) {
-    const upAgent = database.read(agentModel, {id:req.param.id}).first();
+    const upAgent = database.read(agentModel, {id:parseInt(req.param.id)}).first();
     if (!upAgent) {
       return res.status(404).send({ message: 'agent not found' });
     }
-    const updatedAgent = database.using(upAgent.merge(req.body)).write('update', {id:req.param.id});
+    const updatedAgent = database.using(upAgent.merge(req.body)).write('update', {id:parseInt(req.param.id)});
     return res.status(200).send(updatedAgent);
   },
 
 
   delete(req, res) {
-    const delAgent = database.read(agentModel, {id:req.param.id}).first();
+    const delAgent = database.read(agentModel, {id:parseInt(req.param.id)}).first();
     if (!delAgent) {
       return res.status(404).send({ message: 'agent not found' });
     }
-    const isEmpty = Boolean(database.delete(agentModel, {id:req.param.id}));
+    const isEmpty = Boolean(database.delete(agentModel, {id:parseInt(req.param.id)}));
     return res.status(204).send({del:true, empty:isEmpty});
   },
 };
